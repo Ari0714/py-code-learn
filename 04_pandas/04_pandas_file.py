c34741cf0a97882b85bbf4359cf544ee9f935dfc
chenjie <1187334030@qq.com>
2024-08-27 18:17:58 +0800

    Changes by firm laptop

--- 04_pandas/04_pandas_file.py
+++ 04_pandas/04_pandas_file.py
@@ -3,10 +3,18 @@ from pyspark.sql import SparkSession, DataFrame
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import jieba
+
+
+def cut_words(word):
+    return list(jieba.cut(word))
 
 if __name__ == '__main__':
     pd1 = pd.read_csv('00_input/data.csv', sep='\t', header=0)
     print(pd1)
+    print(pd1.info())
+    print(pd1.city)
+    print("*" * 20)
     print(pd1['city'])
     print(pd1[0:1])
     print(pd1['city'][0:1])
@@ -35,5 +43,18 @@ if __name__ == '__main__':
     print(pd1_query)
 
     # write
+    # print("*" * 20)
+    # pd1_query.to_csv('00_input/output.csv',sep=',',header=True,index=False)
+
+    # drop duplicate
     print("*" * 20)
-    pd1_query.to_csv('00_input/output.csv',sep=',',header=True,index=False)
+    pd1_unique = pd1.id.unique()
+    print(len(pd1_unique))
+    drop_duplicate = pd1.duplicated(subset='id',keep='first')
+
+    # apply
+    pd1_apply = pd1.advantage.apply(cut_words)
+    print(pd1_apply)
+
+
+
