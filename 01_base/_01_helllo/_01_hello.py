6def0fb5f76ed67b180a4f570af45fa1f9ce5923
chenjie <1187334030@qq.com>
2024-09-04 19:28:05 +0800

    Changes by firm laptop

--- 01_base/_01_helllo/_01_hello.py
+++ 01_base/_01_helllo/_01_hello.py
@@ -1,3 +1,14 @@
-
+import  datetime
+import time
 print("hello world")
 
+
+'''time'''
+print(time.time())
+print(time.localtime())
+
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+
+
+# dt = time.strptime('%Y-%m-%d %H:%M:%S', time.time())
+# print(dt)
--- 06_hdp_base/04_hdfs/hdfs_test.py
+++ 06_hdp_base/04_hdfs/hdfs_test.py
@@ -1,21 +1,13 @@
-import findspark
-import pandas as pd
 
-findspark.init()
-from datetime import datetime, timedelta
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, DecimalType, FloatType
-from pyspark.sql import SparkSession
 import jieba
-import pyhdfs
+# import pyhdfs
 from hdfs3 import HDFileSystem
+import sys
 
 if __name__ == '__main__':
-    client = pyhdfs.HdfsClient(hosts='hdp:9870',user_name='hdfs')
-    dir = client.listdir('/')
-    print(client.copy_from_local('../00_input/data.csv', '/input'))
-    for i in dir:
-        print(i)
-    # print(dir)
 
-    print(pd.DataFrame(dir,columns=['name']))
+    hdfs = HDFileSystem(host='dmp-64', port=8020)
+    hdfs.ls('/user/data')
+    # # hdfs.put('local-file.txt', '/user/data/remote-file.txt')
+    # # hdfs.cp('/user/data/file.txt', '/user2/data')
 
