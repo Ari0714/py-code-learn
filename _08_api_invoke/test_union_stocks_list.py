a1fa80fee753d99db33b562bac10ab4acd03e001
ari.chen <ari.chen.cn@gmail.com>
2026-04-09 09:33:05 +0800

    chore: add history data-chart

--- _08_api_invoke/test_union_stocks_list.py
+++ _08_api_invoke/test_union_stocks_list.py
@@ -1,10 +1,10 @@
 
-# 30 stocks
+# 29 stocks
 stock_list = [
     "spy", "qqq", "smh", "gld",
     "nvda", "goog", "tsla", "aapl", "meta",
     "amd", "tsm", "avgo", "aehr", "mu", "lite", "aaoi",
-    "iren", "cifr", "nbis", "wulf", "clsk",
+    "iren", "cifr", "nbis", "wulf",
     "rklb", "asts", "onds",
     "be", "te", "oklo", "uuuu",
     "hood", "pltr"]
@@ -13,7 +13,7 @@ stocks_blank = [
     "spy", "qqq", "smh", "gld", "",
     "nvda", "goog", "tsla", "aapl", "meta", "", "",
     "amd", "tsm", "avgo", "aehr", "mu", "lite", "aaoi", "",
-    "iren", "cifr", "nbis", "wulf", "clsk", "", "",
+    "iren", "cifr", "nbis", "wulf", "", "",
     "rklb", "asts", "onds", "", "",
     "be", "te", "oklo", "uuuu", "", "",
     "hood", "pltr"]
